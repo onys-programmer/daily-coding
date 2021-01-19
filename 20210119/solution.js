1437e3f6f81c30521c886ac388ae77fc4a311743
thkim <thkim2017@gmail.com>
2021-01-19 20:52:51 +0900

    Make solutions be passed solution1: Used for statement, slice() and sort() solution2: Used map(), filter() and sort()

--- 20210119/solution.js
+++ 20210119/solution.js
@@ -1,19 +1,22 @@
 function solution(array, commands) {
   const result = [];
+  if (array.length <= 100) {
+    
+    for (let x = 0; x < commands.length; x++) {
+      const i = commands[x][0];
+      const j = commands[x][1];
+      const k = commands[x][2];
 
-  for (let x = 0; x < commands.length; x++) {
-    const i = commands[x][0];
-    const j = commands[x][1];
-    const k = commands[x][2];
+      const slicedArray = array.slice(i - 1, j);
 
-    const slicedArray = array.slice(i - 1, j);
+      const sortedArray = slicedArray.sort((a,b) => (a-b));
 
-    const sortedArray = slicedArray.sort();
-
-    result[x] = sortedArray[k - 1];
+      result[x] = sortedArray[k - 1];
+    }
+    return result;
+  } else {
+    return false;
   }
-
-  return result;
 }
 
 module.exports = solution;
