78dfcb6dc31bc49139e2067577a083ef3a58c1d8
thkim <thkim2017@gmail.com>
2021-01-24 21:20:29 +0900

    Apply Map But failed

--- 20210124/solution.js
+++ 20210124/solution.js
@@ -1,26 +1,32 @@
 function solution(answers) {
-  const result = [];
-  
-  const pattern1 = [ 1, 2, 3, 4, 5];
-  const pattern2 = [ 2 , 1, 2, 3, 2, 4, 2, 5 ];
-  const pattern3 = [ 2, 1, 2, 3, 2, 4, 2, 5 ];
-  const patterns = [ pattern1, pattern2, pattern3 ];
+  const bestScorers = [];
+
+  const pattern1 = [1, 2, 3, 4, 5];
+  const pattern2 = [2, 1, 2, 3, 2, 4, 2, 5];
+  const pattern3 = [2, 1, 2, 3, 2, 4, 2, 5];
+  const patterns = [pattern1, pattern2, pattern3];
 
   const scores = [];
-  
+
   patterns.forEach((pattern) => {
-    scores.push(answers.filter((a,i) => a === pattern[i % pattern.length]).length);
-})
-  
-  const max = Math.max(scores[0], scores[1], scores[2]);
+    scores.push(answers.filter((a, i) => a === pattern[i % pattern.length]).length);
+  })
 
-  scores.forEach((score) => {
-    if(score == max) {
-      result.push(scores.indexOf(score)+1);
+  const bestScore = Math.max(scores[0], scores[1], scores[2]);
+
+  let map = new Map(); //{key:score, value:index}
+
+  for (let i = 0; i < scores.length; i++) {
+    if (bestScore == scores[i]) {
+      map.set(bestScore, i + 1);
     }
-  });
+  }
 
-  return result;
+  for (let value of map.values()) {
+    bestScorers.push(value);
+  }
+
+  return bestScorers;
 }
 
 module.exports = solution;
