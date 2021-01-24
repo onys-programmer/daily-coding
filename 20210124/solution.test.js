ffdd519e0990e4823bf7ba18e4907bc94b655d5b
thkim <thkim2017@gmail.com>
2021-01-24 15:38:35 +0900

    Set environment Set npm, jest, yesterday's solution

---
+++ 20210124/solution.test.js
@@ -0,0 +1,8 @@
+test('solution',() => {
+
+  const solution = require('./solution');
+
+  expect(solution([ 1, 2, 3, 4, 5 ])).toStrictEqual([1]);
+  expect(solution([ 1, 3, 2, 4, 2 ])).toStrictEqual([ 1, 2, 3]);
+
+})
