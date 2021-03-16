a1f234b44c47c7741b216c4117a08bf882b2b37d
thkim <thkim2017@gmail.com>
2021-03-17 00:30:51 +0900

    Implement modifyArray method Make notSelfNumber index 0

--- 20210316/SelfNumber/src/main/java/Main.java
+++ 20210316/SelfNumber/src/main/java/Main.java
@@ -1,19 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    int[] modifyArray(int[] array) {
-        array
+    int[] modifyArray() {
+        int[] array = new int[10001];
+        int temp;
+        for(int i = 1; i <= 10000; i++) {
+            if(i < 10) {
+                temp = i + i;
+                array[temp] = 1;
+            } else if(i < 100) {
+                temp = i + (i/10) + (i%10);
+                array[temp] = 1;
+            } else if(i < 1000) {
+                temp = i + (i/100) + ((i%100)/10) + ((i%100)%10);
+                array[temp] = 1;
+            } else {
+                temp = i + (i/1000) + ((i%1000)/100) + (((i%1000)%100)/10) + (((i%1000)%100)%10);
+                array[temp] = 1;
+            }
+        }
 
         return array;
     }
 
     public static void printSelfNumber(int[] selfNumbers) {
-        for(int i = 0; i < 10000; i++) {
-            System.out.println(selfNumbers);
+        for(int i = 1; i <= 10000; i++) {
+            System.out.println(selfNumbers[i]);
         }
     }
 
     public static void main(String[] args) {
         printSelfNumber();
     }
-
-
 }
