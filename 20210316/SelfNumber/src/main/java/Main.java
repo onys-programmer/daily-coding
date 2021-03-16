cd74c38777449aa96b600cace4e729f6c73ea2a5
thkim <thkim2017@gmail.com>
2021-03-16 23:59:56 +0900

    Hi

---
+++ 20210316/SelfNumber/src/main/java/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    int[] modifyArray(int[] array) {
+        array
+
+        return array;
+    }
+
+    public static void printSelfNumber(int[] selfNumbers) {
+        for(int i = 0; i < 10000; i++) {
+            System.out.println(selfNumbers);
+        }
+    }
+
+    public static void main(String[] args) {
+        printSelfNumber();
+    }
+
+
+}
