1fc626936ac5ca76ef497a17a13fd9c03d26c6ac
thkim <thkim2017@gmail.com>
2021-04-01 23:53:23 +0900

    Set environment

---
+++ 20210401/solution.js
@@ -0,0 +1,16 @@
+function solution(new_id) {
+  // 1. 3 <= id <= 15
+  // 2. 'a', '1', '-', '_', '.', 
+  // 3. '.' != first && '.' != end 
+  // 4. if(index(n)='.') {
+  //  index(n-1) != ',' && index(n+1) != ','
+  // }
+  const lowerId = new_id.toLowerCase();
+  
+  
+
+}
+
+test('solution', () => {
+
+});
