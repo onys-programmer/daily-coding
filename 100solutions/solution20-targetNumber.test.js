fda95993f95ec5f40ae2775a0c281f988f147dda
thkim <thkim2017@gmail.com>
2021-04-20 23:06:33 +0900

    Dfs target number - solution20

---
+++ 100solutions/solution20-targetNumber.test.js
@@ -0,0 +1,23 @@
+test('target number', () => {
+  function solution(numbers, target) {
+    let answer = 0;
+
+    function recur(idx, sum) {
+      if( idx === numbers.length) {
+        if(sum === target) {
+          answer+=1;
+        }
+        return;
+      }
+
+      recur(idx+1, sum + numbers[idx]);
+      recur(idx+1, sum - numbers[idx]);
+    }
+
+    recur(0, 0);
+
+    return answer;
+  }
+
+  expect(solution([1,1,1,1,1], 3)).toBe(5);
+});
