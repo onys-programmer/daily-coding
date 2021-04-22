8b1771016804ba6e0cc7f3610bd1e7385519ed96
thkim <thkim2017@gmail.com>
2021-04-22 13:41:35 +0900

    greedy - solution28

---
+++ 100solutions/solution28.test.js
@@ -0,0 +1,44 @@
+// 인접 번호에게만 빌려줄수있음
+// 최대한 많은 학생이 체육수업 듣기
+
+// int n = 전체 학생의 수
+// arr lost = 도난당한 번호
+// arr reserve = 여벌 보유 번호
+// return = 체육수업 들을 수 있는 학생 최대값
+test('체육복', () => {
+  function solution(n, lost, reserve) {
+    let temp = reserve.slice();
+    // reserve 배열을 temp에 복사
+
+    for (let i in temp) {
+      let key = lost.indexOf(temp[i])
+      // 원래 reserve[i]값을 가진다면 인덱스 몇? = 을 key에 할당
+      // 키는 여벌옷 가지고 도난당한 사람의 lost index
+      
+      // 그런 사람이 있을 때 실행
+      if(key != -1) {
+        lost.splice(key, 1);
+        // lost에서 도난당한 사람을 지움
+        reserve.splice(reserve.indexOf(temp[i]), 1);
+        // reserve에서도 도난당한 사람 지움
+      }
+
+      // 여벌옷 가지고 도난당한 사람 없으면 아무것도 안함
+    }
+
+    for (let i of reserve) {
+      let key = lost.includes(i-1) ? lost.indexOf(i-1) : lost.indexOf(i+1)
+
+      if (key != -1) {
+        lost.splice(key, 1)
+      }
+    }
+
+    return n - lost.length;
+  }
+
+  expect(solution(5, [2, 4], [1, 3, 5])).toBe(5);
+  expect(solution(5, [2, 4], [2])).toBe(4);
+  expect(solution(5, [2, 4], [3])).toBe(4);
+  expect(solution(3, [3], [1])).toBe(2);
+});
