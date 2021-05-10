cb2a7d7bfcb86af2d6d8f3a7ba65da4a7da7df6f
thkim <thkim2017@gmail.com>
2021-05-10 16:03:48 +0900

    Arrange Directories 2

--- 202103/20210320/SelfNumber/src/main/java/PrintSelfNumber.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public class PrintSelfNumber {
-    public static void printSelfNumber(List<Integer> selfNumbers) {
-        for(int i = 1; i <= 10000; i++) {
-            System.out.println(selfNumbers.get(i));
-        }
-    }
-}
