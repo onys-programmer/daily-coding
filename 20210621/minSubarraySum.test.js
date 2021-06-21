c5d64f0a316e1098b9e870bbcac0afe357a9fc0c
thkim <thkim2017@gmail.com>
2021-06-21 23:16:44 +0900

    Implement maxSubarraySum It uses current value and Math.max method

---
+++ 20210621/minSubarraySum.test.js
@@ -0,0 +1,16 @@
+describe('minSubarrayLen', () => {
+  function minSubarrayLen(array, int) {
+    let length;
+    let min = Infinity;
+
+    for(let i = 0; i < array.length; i++) {
+      
+    }
+
+    return length;
+  }
+
+  it("returns minSubarrayLen's sum", () => {
+    expect(minSubarrayLen([2,3,1,2,4,3], 7)).toBe(2);
+  });
+});
