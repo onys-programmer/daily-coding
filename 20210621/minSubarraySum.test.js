6145b4e07b14862819b29bee71bb08d19ab16c1d
thkim <thkim2017@gmail.com>
2021-06-22 00:34:57 +0900

    Implement minSubArrayLen on my own

--- 20210621/minSubarraySum.test.js
+++ 20210621/minSubarraySum.test.js
@@ -1,16 +1,25 @@
-describe('minSubarrayLen', () => {
-  function minSubarrayLen(array, int) {
-    let length;
-    let min = Infinity;
+describe('minSubArrayLen', () => {
+  function minSubArrayLen(arr, target) {
+    let start = 0;
+    let end = 0;
+    let minLen = Infinity;
+    let total = 0;
 
-    for(let i = 0; i < array.length; i++) {
-      
+    while(start < arr.length) {
+      if(total < target && end < arr.length) {
+        total += arr[end];
+        end++;
+      } else if(total >= target) {
+        minLen = Math.min(minLen, end - start);
+        total -= arr[start];
+        start++;
+      } else if(end >= arr.length) break;
     }
 
-    return length;
+    return minLen === Infinity ? 0 : minLen;
   }
 
-  it("returns minSubarrayLen's sum", () => {
-    expect(minSubarrayLen([2,3,1,2,4,3], 7)).toBe(2);
+  it("returns minSubArrayLen's sum", () => {
+    expect(minSubArrayLen([2,3,1,2,4,3], 7)).toBe(2);
   });
 });
