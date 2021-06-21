d569880f5c0379b5caed15e66e974a1bc399dfb0
thkim <thkim2017@gmail.com>
2021-06-21 21:04:12 +0900

    Try to understand yesterday's code

--- 20210618/isSubsequence.test.js
+++ 20210618/isSubsequence.test.js
@@ -1,59 +1,26 @@
 describe('isSubsequence', () => {
-  function isSubsequence(str1, str2) {
-    if (str1 === str2) return true;
-    let arr1 = str1.split("");
-    let arr2 = str2.split("");
-    let temp = [];
-    
-    // s1 t2 r3 ...으로 객체 배열 만들기
-    let objArr = [];
-
-    for(let i = 0; i < str2.length; i++) {
-      let obj = {};
-      obj[str2[i]] = i;
-      objArr.push(obj);
+function isSubsequence(pattern, text) {
+  let i = 0;
+  for(let j = 0; j < text.length; j++) {
+    if(pattern[i] !== text[j]) {
+      console.log(`i=${i}, j=${j}, pattern[i] : ${pattern[i]}, text[j] = ${text[j]}, continue`);
+      continue;
+    } else if(pattern[i] === text[j]) {
+      console.log(`i=${i}, j=${j} pattern[i] : ${pattern[i]}, text[j] = ${text[j]}, i++`);
+      i++;
     }
-    console.log(objArr);
-
-    // sing을 하나씩 순회
-    // s가 string에 있으면 s를 빼서 push
-    for(let i = 0; i < str1.length; i++) {
-      if(arr2.some(e => e === str1[i])) {
-        // s가 property인 객체를 objArr에서 찾아 빼고, temp에 넣기
-        let obj;
-        for(let i = 0; i < objArr.length; i++) {
-          if(objArr[i].name === i) {
-            obj = objArr[i];
-          }
-          console.log(obj);
-        }
-
-        objArr.splice()
-
-      }
-    }
-
-    console.log(temp);
-    return false;
   }
-
-  // sing, string이면
-  // s1 t2 r3 ...으로 객체 만들기
-  // sing을 하나씩 순회
-  // s가 string에 있으면 s를 빼서 push
-  // tring
-  // i가 tring에 있으면
-  // i 빼
-  // n이 trng에 있으면 n 빼서 Push
-  // g가 trg에 있으면 g 빼서 push
-
-  // 없으면 false return
-
-  // 결과 배열을 검사
-  // 다음거 value가 현재 value보다 크면 true return, 아니면 false
+  return i === pattern.length;
+}
 
   it('checks whether str1 is a subsequence of str2', () => {
-    expect(isSubsequence('', '')).toBe(true);
-    expect(isSubsequence('sing', 'string')).toBe(true);
+    // expect(isSubsequence('hello', 'hello world')).toBe(true);
+    // expect(isSubsequence('', '')).toBe(true);
+    // expect(isSubsequence('sing', 'sting')).toBe(true);
+    expect(isSubsequence('abc', 'rcabacadabra')).toBe(true);
+    // expect(isSubsequence('abc', 'acb')).toBe(false);
+    // expect(isSubsequence('ffj', 'acb')).toBe(false);
   });
 });
+
+// 문제 이해를 잘 해야 함. 미꾸라지처럼 빠져나가서 쉬운 알고리즘 형태를 만들어보자.
