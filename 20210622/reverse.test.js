2001786bcee26294fc409d223931af2a991f33c9
thkim <thkim2017@gmail.com>
2021-06-22 17:11:18 +0900

    Implement colt's reverse

--- 20210622/reverse.test.js
+++ 20210622/reverse.test.js
@@ -1,5 +1,10 @@
 describe('reverse', () => {
   function reverse(str) {
+    if(str.length <= 1) return str;
+    return reverse(str.slice(1)) + str[0];
+  }
+
+  function myReverse(str) {
     let reversedStr = [];
     let arr = str.split("");
 
