9565ec2a02c84cfea638d65ffa7cbb1f67a38e9c
thkim <thkim2017@gmail.com>
2021-06-22 16:58:02 +0900

    Implement findLongestSubstring on my own

--- 20210622/findLongestSubstring.test.js
+++ 20210622/findLongestSubstring.test.js
@@ -1,8 +1,23 @@
 describe('findLongestSubstring', () => {
-  function findLongestSubstring() {
-    let substring = '';
-    
-    return substring.length;
+  function findLongestSubstring(str) {
+    let longest = 0;
+    let seen = {};
+    let start = 0;
+
+    for(let i = 0; i < str.length; i++) {
+      let char = str[i];
+
+      if(seen[char]) {
+        start = Math.max(start, seen[char]);
+      }
+
+
+      seen[char] = i + 1;
+
+      longest = Math.max(longest, i - start + 1);
+    }
+
+    return longest;
   }
 
   it('returns the length of the longest substring with all distinct characters', () => {
@@ -11,3 +26,34 @@ describe('findLongestSubstring', () => {
 
   });
 });
+
+// function findLongestSubstring(str) {
+//   let longest = 0;
+//   let seen = {};
+//   let start = 0;
+
+//   for(let i = 0; i < str.length; i++) {
+//     let char = str[i];
+//     console.log(`char is str[${i}]: ${char}`);
+//     if(seen[char]) {
+//       start = Math.max(start, seen[char]);
+//       console.log(`char is in seen, start becomes greater one between start: ${start} and seen[${char}]: ${seen[char]}`);
+//     }
+
+//     console.log(`longest : ${longest} or i: ${i} - start: ${start} + 1`)
+//     longest = Math.max(longest, i - start + 1);
+
+//     console.log(`seen[char] : (i)${i} + 1`);
+//     seen[char] = i + 1;
+//     console.log(seen);
+//   }
+
+//   return longest;
+// }
+
+
+// thisisawesome
+// this
+// si
+// isawe
+// some
