44dbaaa43817812c210a0d96c98af5e762210e12
onys <thkim2017@gmail.com>
2021-12-04 12:14:54 +0900

    Solve boj 11021

---
+++ 20211204/boj11021/s1.py
@@ -0,0 +1,8 @@
+import sys
+sys.stdin = open('input.txt')
+
+T = int(input())
+for test_case in range(1, T + 1):
+    a, b = map(int, input().split())
+    answer = a + b
+    print(f'Case #{test_case}: {answer}')
