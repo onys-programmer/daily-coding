4249a93efd40995e113dfe314c1cd25b66895353
onys <thkim2017@gmail.com>
2021-12-06 14:46:01 +0900

    Solve boj2562

---
+++ 20211206/boj2562/s1.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open('input.txt')
+
+max_num = [0, 0]
+
+for i in range(1, 10):
+    num = int(input())
+    if num > max_num[0]:
+        max_num[0] = num
+        max_num[1] = i
+
+print(max_num[0])
+print(max_num[1])
