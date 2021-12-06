69f59bc248ccbd40e05027468a127dd325d3db6f
onys <thkim2017@gmail.com>
2021-12-06 15:13:35 +0900

    Solve boj 8958

---
+++ 20211206/boj8958/s1.py
@@ -0,0 +1,17 @@
+import sys
+sys.stdin = open('input.txt')
+
+T = int(input())
+
+for test_case in range(T):
+    score = 0
+    acc_cnt = 1
+    results = input()
+    for result in results:
+        if result == 'O':
+            score += acc_cnt
+            acc_cnt += 1
+        else:
+            acc_cnt = 1
+
+    print(score)
