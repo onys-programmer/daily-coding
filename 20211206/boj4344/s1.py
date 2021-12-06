039c7f2107a51a5e6013e3932c374f29de6687d0
onys <thkim2017@gmail.com>
2021-12-06 15:35:04 +0900

    Solve boj 4344

---
+++ 20211206/boj4344/s1.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('input.txt')
+
+T = int(input())
+
+for test_case in range(T):
+    score_list = list(map(int, input().split()))
+    avg = sum(score_list[1:]) / score_list[0]
+    higher_than_avg = list(filter(lambda score: score > avg, score_list[1:]))
+
+    higher_than_avg_rate = round(len(higher_than_avg) / score_list[0] * 100, 3)
+    dot_idx = str(higher_than_avg_rate).index('.')
+    required_zeros = '0' * (dot_idx + 4 - len(str(higher_than_avg_rate)))
+    print(f'{higher_than_avg_rate}{required_zeros}%')
