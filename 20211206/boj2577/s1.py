aca290dd287f298491027533646ce2def2e63f29
onys <thkim2017@gmail.com>
2021-12-06 14:55:46 +0900

    Solve boj2577

---
+++ 20211206/boj2577/s1.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin = open('input.txt')
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+result = str(a * b * c)
+
+table = [0] * 10
+for char in result:
+    table[int(char)] += 1
+
+for cnt in table:
+    print(cnt)
