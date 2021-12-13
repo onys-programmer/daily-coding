03fe563d06dda1e2a835761cc1f2a4c37f78bd30
onys <thkim2017@gmail.com>
2021-12-13 17:47:04 +0900

    Solve boj10250

---
+++ 20211213/boj10250/s1.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin = open('input.txt')
+
+T = int(input())
+
+for test_case in range(T):
+    H, W, N = map(int, input().split())
+    zero = '0'
+
+    if H == 1:
+        floor_num = 1
+        room_num = N
+    else:
+        floor_num = N % H if N % H != 0 else H
+        room_num = (N - 1) // H + 1
+    if room_num >= 10:
+        zero = ''
+
+    print(f'{floor_num}{zero}{room_num}')
