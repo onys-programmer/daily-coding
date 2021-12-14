6d5b43a67f0c76f82afe542d01dc83d80c5e0419
onys <thkim2017@gmail.com>
2021-12-14 18:03:03 +0900

    Solve boj2798

---
+++ 20211214/boj2798/s1.py
@@ -0,0 +1,29 @@
+import sys
+sys.stdin = open('input.txt')
+
+N, M = map(int, input().split())
+cards = list((map(int, input().split())))
+
+results = []
+cards_sum = 0
+for i in range(N):
+    cards_sum = 0
+    cards_sum += cards[i]
+
+    for j in range(i + 1, N):
+        if j == i:
+            continue
+        cards_sum += cards[j]
+
+        for k in range(j + 1, N):
+            if k == j:
+                continue
+            if k == i:
+                continue
+            cards_sum += cards[k]
+            results.append(M - cards_sum)
+            cards_sum -= cards[k]
+
+        cards_sum -= cards[j]
+
+print(M - list(filter(lambda x: x >= 0, sorted(results)))[0])
