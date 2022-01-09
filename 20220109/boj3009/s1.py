5a016bbddaff72442a8b4ac661049bd17c995188
onys <thkim2017@gmail.com>
2022-01-09 22:18:10 +0900

    Solve boj 3009

---
+++ 20220109/boj3009/s1.py
@@ -0,0 +1,28 @@
+import sys
+sys.stdin = open('input.txt')
+
+dots = []
+for i in range(3):
+    dots.append(list(map(int, input().split())))
+
+i = 0
+for j in range(2):
+    if dots[i][j] == dots[i + 1][j]:
+        dots[i][j] = -1
+        dots[i + 1][j] = -1
+    if dots[i][j] == dots[i + 2][j]:
+        dots[i][j] = -1
+        dots[i + 2][j] = -1
+    if dots[i + 1][j] == dots[i + 2][j]:
+        dots[i + 1][j] = -1
+        dots[i + 2][j] = -1
+
+a, b = 0, 0
+
+for dot in dots:
+    if dot[0] != -1:
+        a = dot[0]
+    if dot[1] != -1:
+        b = dot[1]
+
+print(a, b)
