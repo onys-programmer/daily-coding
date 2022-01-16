17451a439105e06af8b78fa746813db80a8a1c56
onys <thkim2017@gmail.com>
2022-01-16 20:11:58 +0900

    Solve boj2576

---
+++ 20220116/boj2576/s1.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open('input.txt')
+
+arr = []
+for _ in range(7):
+    n = int(input())
+    arr.append(n)
+odds = list(filter(lambda x: x % 2 != 0, arr))
+if len(odds) != 0:
+    print(sum(odds))
+    print(min(odds))
+else:
+    print(-1)
