e4b023ac99e543d3f9d970ec1f974bc38898c707
onys <thkim2017@gmail.com>
2022-01-22 11:55:55 +0900

    Solve boj2875

---
+++ 20220122/boj2875/s1.py
@@ -0,0 +1,17 @@
+import sys
+sys.stdin = open('input.txt')
+
+girls, boys, intern = map(int, input().split())
+
+teams = 0
+while girls and boys:
+    if girls + boys - 3 < intern:
+        break
+    if girls < 2 or boys < 1:
+        break
+
+    girls -= 2
+    boys -= 1
+    teams += 1
+
+print(teams)
