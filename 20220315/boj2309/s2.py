a2422f8f1f675439ab056a8cb0b11cb96a1bd134
onys <thkim2017@gmail.com>
2022-03-15 17:02:51 +0900

    boj2309 refactoring

--- 20220315/boj2309/s2.py
+++ 20220315/boj2309/s2.py
@@ -2,13 +2,8 @@ import sys
 from itertools import permutations
 sys.stdin = open('input.txt')
 
-heights = []
-for _ in range(9):
-    height = input()
-    heights.append(int(height))
-
-sortedHeights = sorted(heights)
-for permutation in permutations(sortedHeights, 7):
+heights = [int(input()) for _ in range(9)]
+for permutation in permutations(sorted(heights), 7):
     if sum(permutation) == 100:
         for ele in permutation:
             print(ele)
