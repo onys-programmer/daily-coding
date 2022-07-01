b3d9c032a6d749f1a218666e3778657f53146367
onys <thkim2017@gmail.com>
2022-07-01 23:30:12 +0900

    feat: 🎸 11655

---
+++ 20220630/boj11655/s1.js
@@ -0,0 +1,4 @@
+const input = require('fs').readFileSync('input.txt').toString().trim();
+console.log(input);
+const alphas = 'abcdefghijklmnopqrstuvwxyz';
+input.split("").map(alphas[str] => alphas[str + 13]);
